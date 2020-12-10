2af9d0c6eece3d648082119bfff8f571bfb49e18
WindigoTG <windigotp@gmail.com>
2020-12-10 23:52:16 +0800

    ДЗ урок №2

---
+++ My First Asset Flip/Assets/Scripts/CameraSwitching.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitching : MonoBehaviour
+{
+    GameObject[] cameraList;
+    [SerializeField] GameObject localCamera;
+    [SerializeField] GameObject player;
+
+    void Start()
+    {
+        cameraList = GameObject.FindGameObjectsWithTag("VirtCam");
+        foreach (GameObject cam in cameraList)
+            Debug.Log(cam.name);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            foreach (GameObject cam in cameraList)
+                    cam.GetComponent<Cinemachine.CinemachineVirtualCamera>().Priority = 10;
+            localCamera.GetComponent<Cinemachine.CinemachineVirtualCamera>().Priority = 99;
+        }
+    }
+}
